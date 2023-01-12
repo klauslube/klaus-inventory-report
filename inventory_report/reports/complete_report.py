b680a9b4d1a0dcdb39d3ecea1eab13b472208357
klauslube <klauslube93@gmail.com>
2023-01-12 19:51:37 -0300

    feat: implementation of complete report class

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,37 @@
-class CompleteReport:
-    pass
+from datetime import date
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(data: list):
+        oldest_manufacturing_date = str(date.max)
+        neareast_expiration_date = str(date.max)
+        company_products = {}
+
+        company_with_most_products = Counter(
+            [product["nome_da_empresa"] for product in data]
+        ).most_common(1)[0][0]
+    
+        for product in data:
+            if product["data_de_fabricacao"] < oldest_manufacturing_date:
+                oldest_manufacturing_date = product["data_de_fabricacao"]
+
+            if product["data_de_validade"] < neareast_expiration_date:
+                neareast_expiration_date = product["data_de_validade"]  
+            if product["nome_da_empresa"] in company_products:
+                company_products[product["nome_da_empresa"]] += 1
+            else:
+                company_products[product["nome_da_empresa"]] = 1
+
+        companies = ""
+        for company, quantity in company_products.items():
+            companies += f"- {company}: {quantity}\n"
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
+            f"Data de validade mais próxima: {neareast_expiration_date}\n"
+            f"Empresa com mais produtos: {company_with_most_products}"
+            "Produtos estocados por empresa:\n"
+            f"{companies}"
+        )
