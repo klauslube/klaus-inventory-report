c44e4a2d6b9f1575cf52505fb9f982a9c8f5ab76
klauslube <klauslube93@gmail.com>
2023-01-12 16:59:45 -0300

    test: verifica a instancia da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,14 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui 
+    product = Product(
+        1, "leite", "gloria", "06-12-2022", "06-01-2023", "0123", "seco" 
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "leite"
+    assert product.nome_da_empresa == "gloria"
+    assert product.data_de_fabricacao == "06-12-2022"
+    assert product.data_de_validade == "06-01-2023"
+    assert product.numero_de_serie == "0123"
+    assert product.instrucoes_de_armazenamento == "seco"
