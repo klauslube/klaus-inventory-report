86577ad3d12f66b5dfbe94a6efc2058e541b7e7a
klauslube <klauslube93@gmail.com>
2023-01-13 16:16:20 -0300

    refactor: complete report less complex

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,24 +1,12 @@
-from datetime import date
-from collections import Counter
 from inventory_report.reports.simple_report import SimpleReport
 
 
-class CompleteReport(SimpleReport):
+class CompleteReport:
     def generate(data: list):
-        oldest_manufacturing_date = str(date.max)
-        neareast_expiration_date = str(date.max)
+        simple_Report = SimpleReport.generate(data)
         company_products = {}
 
-        company_with_most_products = Counter(
-            [product["nome_da_empresa"] for product in data]
-        ).most_common(1)[0][0]
-
         for product in data:
-            if product["data_de_fabricacao"] < oldest_manufacturing_date:
-                oldest_manufacturing_date = product["data_de_fabricacao"]
-
-            if product["data_de_validade"] < neareast_expiration_date:
-                neareast_expiration_date = product["data_de_validade"]
             if product["nome_da_empresa"] in company_products:
                 company_products[product["nome_da_empresa"]] += 1
             else:
@@ -29,9 +17,7 @@ class CompleteReport(SimpleReport):
             companies += f"- {company}: {quantity}\n"
 
         return (
-            f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
-            f"Data de validade mais próxima: {neareast_expiration_date}\n"
-            f"Empresa com mais produtos: {company_with_most_products}"
+            f"{simple_Report}"
             "Produtos estocados por empresa:\n"
             f"{companies}"
         )
