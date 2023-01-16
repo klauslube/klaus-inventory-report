bd53071572f11f47400c17bbcd9bd126e9f3d44e
klauslube <klauslube93@gmail.com>
2023-01-16 19:05:49 -0300

    feat: implementation of abstract method on json file

---
+++ inventory_report/importer/json_importer.py
@@ -0,0 +1,12 @@
+from inventory_report.importer.importer import Importer
+
+import json
+
+
+class JsonImporter(Importer):
+    @classmethod
+    def import_data(self, path):
+        if ".json" not in path:
+            raise ValueError("Arquivo não suportado")
+        with open(path, 'r') as file:
+            return json.load(file)
