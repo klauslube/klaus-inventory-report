466c4ab00c1e2c54b7edb45ab0bc1ac8835f253c
klauslube <klauslube93@gmail.com>
2023-01-16 14:56:09 -0300

    feat: import reports e implement class invetory

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,7 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Invetory:
+  @staticmethod
+  def import_data(path:str, type:str):
